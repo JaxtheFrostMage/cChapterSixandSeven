b4701eb7894d3d80929208658bd70f0281b9b780
Evan Raker <er306632@students.davenport.k12.ia.us>
2018-09-28 11:16:52 -0500

    working

--- rockPaperScissors/rockPaperScissors/Source.cpp
+++ rockPaperScissors/rockPaperScissors/Source.cpp
@@ -12,19 +12,42 @@ int main()
 {
 	int playerChoice;
 	int computerChoice;
+	int again;
 
+	do {
+		cout << "pick Rock, Paper, or Scissors and beat the computer! \n 1 = Rock n\ 2 = Paper n\ 3 = Scissors " << endl;
+		cin >> playerChoice;
 
-	cout << "pick Rock, Paper, or Scissors and beat the computer! \n 1 = Rock n\ 2 = Paper n\ 3 = Scissors " << endl;
-	cin >> playerChoice;
+		srand(time(NULL));
+		computerChoice = rand() % 3 + 1;
 
-	srand(time(NULL));
-	computerChoice = rand() % 3 + 1;
+		if (playerChoice == computerChoice) {
+			cout << "draw...";
+		}
+		else if (playerChoice == 1 || computerChoice == 2) {
+			cout << "Paper beats rock \n I win" << endl;
+		}
+		else if (playerChoice == 1 || computerChoice == 3) {
+			cout << "Rock beats scissors \n You win" << endl;
+		}
+		else if (playerChoice == 2 || computerChoice == 1) {
+			cout << "Paper beats rock \n You win" << endl;
+		}
+		else if (playerChoice == 2 || computerChoice == 3) {
+			cout << "Scissors beats paper \n I win" << endl;
+		}
+		else if (playerChoice == 3 || computerChoice == 1) {
+			cout << "Rock beats Scissors \n I win" << endl;
+		}
+		else if (playerChoice == 3 || computerChoice == 2) {
+			cout << "Scissors beats paper \n You win" << endl;
+		}
 
-	if (playerChoice = computerChoice) {
-		cout << "draw...";
-	}
-	else if (playerChoice == 1 || computerChoice == 2) {
-		cout << "Paper beats rock \n I win" << endl;
-	}
+		cout << "would you like to play again? " << endl;
+		cin >> again;
 
+	} while (again == 1);
+
+	system("pause");
+	return 0;
 }
